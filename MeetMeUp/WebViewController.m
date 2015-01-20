3a9b44bef43e5f1de17dc62f51497ee3f53e258b
Gabriel_Azevedo <gabriel.azevedo95@icloud.com>
2015-01-19 19:47:08 -0800

    Fourth Commit

--- MeetMeUp/WebViewController.m
+++ MeetMeUp/WebViewController.m
@@ -8,8 +8,9 @@
 
 #import "WebViewController.h"
 
-@interface WebViewController ()
+@interface WebViewController () <UIWebViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 
 @end
 
@@ -18,10 +19,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.webView.delegate = self;
 
     NSURL *url = [NSURL URLWithString:self.url];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:urlRequest];
+}
+
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+    [self.spinner startAnimating];
+    self.spinner.hidden = false;
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [self.spinner stopAnimating];
+    self.spinner.hidden = true;
+}
+- (IBAction)onForwardButtonPressed:(UIButton *)sender
+{
+    [self.webView goForward];
+}
+- (IBAction)onBackButtonPressed:(UIButton *)sender
+{
+    [self.webView goBack];
 }
 
 
